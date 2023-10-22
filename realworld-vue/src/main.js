3cf11c5021e2d03891472f713f05835d3e568764
simbokyung98 <synk98@gmail.com>
2023-10-23 07:44:45 +0900

    fix : 로그인 구현

--- realworld-vue/src/main.js
+++ realworld-vue/src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/index.js';
 import axios from "axios";
 
 
-
+const pinia = createPinia()
 const app = createApp(App)
+
 app.config.globalProperties.axios = axios
+app.use(pinia)
 app.use(router)
 app.mount('#app')
