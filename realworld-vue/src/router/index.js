764028a85923f1f9dc2ab8df50158acad3388e28
simbokyung <synk98@gmail.com>
2023-10-24 18:08:15 +0900

    수정 : 로그인 권한 구현중( 라우터에서 beforeEach)

--- realworld-vue/src/router/index.js
+++ realworld-vue/src/router/index.js
@@ -1,21 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router';
-
+import { createRouter, createWebHistory } from "vue-router";
+import { useUserStore } from "../store/user";
+import { storeToRefs } from "pinia";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes:[
+  routes: [
     {
-      path:'/',
-      component:() => import("../views/Home.vue")
+      path: "/",
+      component: () => import("../views/Home.vue"),
     },
     {
-      path:'/login',
-      component:() => import("../views/Login.vue")
+      path: "/login",
+      component: () => import("../views/Login.vue"),
     },
     {
-      path:'/register',
-      component:() => import("../views/Register.vue")
+      path: "/register",
+      component: () => import("../views/Register.vue"),
     },
-  ]
-})
+  ],
+});
+
+router.beforeEach((to, from) => {
+  const store = useUserStore();
+  const token = localStorage.getItem("jwt-token");
+
+  const { isLogined, getUserInfo } = storeToRefs(store);
+
+  if (isLogined.value) {
+    return true;
+  } else {
+  }
+});
 export default router;
